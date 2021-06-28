e25f1dafae1da31255610b967ca6dcd9c5e0bcff
RuanMedTI <rurato2002@gmail.com>
2021-06-28 20:09:10 -0300

    tbcompromisso publish

---
+++ ToDoListAcademia.DataBase/TBCompromisso.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[TBCompromisso] (
+    [Id]              INT           NOT NULL,
+    [Assunto]         VARCHAR (200) NULL,
+    [Local]           VARCHAR (50)  NULL,
+    [DataCompromisso] DATETIME      NULL,
+    [HoraInicio]      VARCHAR (50)  NULL,
+    [DataTermino]     DATETIME      NULL,
+    CONSTRAINT [PK_TBCompromisso] PRIMARY KEY CLUSTERED ([Id] ASC)
+);
