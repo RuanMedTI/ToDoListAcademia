6c38c4c3cfa8882f04ee93b0a37be1b214797b73
RuanMedTI <rurato2002@gmail.com>
2021-06-29 15:31:05 -0300

    commit

--- ToDoListAcademia.DataBase/dbo/Tables/TBCompromisso.sql
+++ ToDoListAcademia.DataBase/dbo/Tables/TBCompromisso.sql
@@ -5,5 +5,8 @@
     [DataCompromisso] DATETIME      NULL,
     [HoraInicio]      VARCHAR (50)  NULL,
     [DataTermino]     DATETIME      NULL,
+    [Id_Contato] INT NULL CONSTRAINT [FK_TBCompromisso_TBContatos]
+		FOREIGN KEY ([Id_Contato]) REFERENCES [dbo].[TBContatos] ([Id])
+		ON DELETE SET NULL
     CONSTRAINT [PK_TBCompromisso] PRIMARY KEY CLUSTERED ([Id] ASC)
 );
